f548a09272620a67a69d338cdc93c41bdec2afba
datewu <hihahajun@gmail.com>
2024-06-07 17:31:32 +0800

    options and errors

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -6,6 +6,7 @@
 // the `total_cost` function will calculate the total cost of the tokens. Since
 // the player typed in the quantity, though, we get it as a string-- and they
 // might have typed anything, not just numbers!
+// "1sdfsdf334".parse<i32>() method
 //
 // Right now, this function isn't handling the error case at all (and isn't
 // handling the success case properly either). What we want to do is: if we call
@@ -19,16 +20,17 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
-
-    Ok(qty * cost_per_item + processing_fee)
+    // let qty = item_quantity.parse::<i32>()?; // error proprgate
+    let qty = item_quantity.parse::<i32>(); // error proprgate
+    match qty {
+        Ok(qty) => Ok(qty * cost_per_item + processing_fee),
+        Err(e) => Err(e),
+    }
 }
 
 #[cfg(test)]
