ab659317c6f84e9fd44bbd977c077513033a6ec8
datewu <hihahajun@gmail.com>
2024-06-17 16:46:43 +0800

    add example hello_world

---
+++ src/bin/hello_world.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // println!("Hello World");
+    // println!("I'm a Rustacean!");
+    let output = format!("{}\n{}", "Hello World!", "I'm a Rustacean!");
+    println! {"{output}"};
+}
